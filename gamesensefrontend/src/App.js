f00843643c005ca14b7011e9b81873ea5b67b9e3
jesuismoi123 <89119350+jesuismoi123@users.noreply.github.com>
2021-12-30 17:53:53 -0500

    navbar
    added navbar

--- gamesensefrontend/src/App.js
+++ gamesensefrontend/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import Footer from'./Components/Footer';
+import Navbar from './Components/Navbar/Navbar';
+
+
 
 
 function App() {
   return (
     <div className="page-container">
       <div className='content-wrap'>
-      
+      <Navbar />
       
       </div>
-      
+
       <div>
       <Footer />
       </div>
