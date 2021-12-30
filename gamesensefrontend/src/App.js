d4b0ed30130fee2a1f25ff57403a3eff090269e6
jesuismoi123 <89119350+jesuismoi123@users.noreply.github.com>
2021-12-30 17:59:35 -0500

    Revert "navbar"

--- gamesensefrontend/src/App.js
+++ gamesensefrontend/src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import './App.css';
 import Footer from'./Components/Footer';
-import Navbar from './Components/Navbar/Navbar';
-
-
 
 
 function App() {
   return (
     <div className="page-container">
       <div className='content-wrap'>
-      <Navbar />
+      
       
       </div>
-
+      
       <div>
       <Footer />
       </div>
